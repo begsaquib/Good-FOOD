ab228a6083d3e00ca27b7640369c17fbe9024ea6
begsaquib <saquibbeg4444@gmail.com>
2024-09-07 18:44:04 +0530

    menulist tailwind

--- src/component/Itemlist.jsx
+++ src/component/Itemlist.jsx
@@ -1,5 +1,32 @@
+import { Rescrd_Url } from "../utils/constants";
 const Itemlist = ({ items }) => {
   console.log(items);
-  return <div>categories</div>;
+  return (
+    <div>
+      {items.map((item) => (
+        
+          <div
+            key={item?.card?.info?.id}
+            className="p-2 m-2  border-gray-300 border-b-2 text-left"
+          >
+            <img
+              className="w-14"
+              src={Rescrd_Url + item?.card?.info?.imageId}
+            />
+            <div className="py-2">
+              <span>{item?.card?.info?.name} </span>
+              <span>
+                - ₹
+                {item?.card?.info?.price
+                  ? item?.card?.info?.price / 100
+                  : item?.card?.info?.defaultPrice}
+              </span>
+            </div>
+            <p className="text-xs">{item?.card?.info?.description}</p>
+         
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Itemlist;
--- src/component/ResCategory.jsx
+++ src/component/ResCategory.jsx
@@ -4,15 +4,15 @@ const ResCategory = ({ data }) => {
   console.log(data);
   return (
     <div>
-      <div>
-        <div className="w-6/12 bg-gray-100 mx-auto my-4 p-4 shadow-lg flex justify-between ">
+      <div className="w-6/12 bg-gray-100 mx-auto my-4 p-4 shadow-lg  ">
+        <div className="flex justify-between">
           <span className="font-bold">
             {data.title} ({data.itemCards.length})
           </span>
           <span>⬇️</span>
         </div>
-      </div>
       <Itemlist items={data.itemCards} />
+      </div>
     </div>
   );
 };
