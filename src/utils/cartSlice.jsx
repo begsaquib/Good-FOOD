c69ec2f00da21702affb8e8c6db129de8c75ae86
begsaquib <saquibbeg4444@gmail.com>
2024-09-12 11:02:04 +0530

    few updates

--- src/utils/cartSlice.jsx
+++ src/utils/cartSlice.jsx
@@ -1,24 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartSlice= createSlice({
-    name: 'cart',
-    initialState:{
-        items:[]
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      //mutating our state here
+      state.items.push(action.payload);
     },
-    reducers:{
-        addItem:(state,action)=>{
-            //mutating our state here
-         state.items.push(action.payload)
-        },
-        removeItem:(state)=>{
-            state.items.pop()
-        },
-        clearItem:(state)=>{
-            state.items.length=0
-        },
-
+    removeItem: (state, action) => {
+      // Removing item from cart by its unique id
+      state.items = state.items.filter(
+        (item) => item.card.info.id !== action.payload.card.info.id
+      );
     },
-})
+    clearItem: (state) => {
+      state.items.length = 0;
+    },
+  },
+});
 
-export const{ addItem, removeItem, clearItem}=cartSlice.actions
-export default cartSlice.reducer
+export const { addItem, removeItem, clearItem } = cartSlice.actions;
+export default cartSlice.reducer;
